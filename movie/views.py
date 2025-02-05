04b12f71bbd30b715e1b82f03c240f6aa592fb56
Mvanegas1505 <mvanegaso1@eafit.edu.co>
2025-02-05 10:37:50 -0500

    Changes in Home Function

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name': 'Nathan'})
+    return render(request, 'home.html', {'name': 'Martin Vanegas'})
 
 def about (request):
     return HttpResponse('<h1>HOLAAA!!!!</h1>')
